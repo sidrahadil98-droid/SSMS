f9de42927b0a0828d8db12f62adc689f70b85932
Sidrah Adil <sidrahadil98@gmail.com>
2026-09-19 12:54:10 +0500

    Update Search-Like-IN-Orderby- QUERY

--- CLASS-1.sql
+++ CLASS-1.sql
@@ -17,7 +17,7 @@ CREATE TABLE Employee(
 );
 
 --Single Record Insert 
-INSERT INTO Employee VALUES('Ali', 'Software Developer', 50000, 'Karachi', null);
+INSERT INTO Employee VALUES('Hania', 'Full-Stack Developer', 125000, 'Hyderabad', null);
 
 --Read Table record
 SELECT * FROM Employee;
@@ -46,7 +46,7 @@ Update Employee set empName= 'Tahir', designation='Product Manager' where id=2;
 -- srf Product Manager ka data show hu
 SELECT empName, designation from Employee where designation='Product Manager';
 
--- where not Product Manager k alawa sa ka data show hu
+-- where not Product Manager k alawa sab ka data show hu
 SELECT empName, designation from Employee where not designation='Product Manager';
 
 -- AND
@@ -57,8 +57,46 @@ AND salary<=88000;
 SELECT empName, designation, salary from Employee where not designation='Product Manager'
 OR salary > 88000;
 
+SELECT id, empName, designation, salary from Employee where not designation='Product Manager'
+OR salary > 88000;
+
 SELECT id, empName, designation, salary from Employee where designation='Product Manager'
 OR salary < 88000;
 
 -- between (2 se 6 tak ka record show hu) 
 SELECT * FROM Employee where id between 2 and 6;
+
+-- Like && Search Query %%
+Select * FROM Employee where city like '%Ka%';
+
+Select * FROM Employee where designation like '%Mo%';
+
+Select * FROM Employee where city like '%Mu%' AND designation like '%Senior%' ;
+
+-- IN 
+Select * FROM Employee where city IN ('Islamabad');
+
+-- ORDER BY
+Select * FROM Employee order by empName asc;
+
+Select * FROM Employee order by salary desc;
+
+-- Top 4 
+Select top 4 * FROM Employee ;
+
+Select top 70 percent * FROM Employee ;
+
+-- aggregate function
+SELECT COUNT(empName) as total_employees FROM Employee;
+
+SELECT MIN(salary) as min_salary FROM Employee;
+
+SELECT Max(salary) as max_salary FROM Employee;
+
+SELECT SUM(salary) as total_salary FROM Employee;
+
+SELECT AVG(salary) as avg_salary FROM Employee;
+
+SELECT AVG(salary) as avg_salary FROM Employee;
+
+SELECT CONCAT(empName, 'has a designation of' , designation, 'has a salary of ', salary) as EmpDetails FROM Employee;
